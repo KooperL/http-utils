3df12a4b408e52f36322a43168a06e994e02ca6a
KooperL <kooper.lingohr@gmail.com>
2024-06-13 19:07:00 +1000

    Add identifier-generator

--- README.md
+++ README.md
@@ -3,7 +3,8 @@
 HTTP wrapper for JavaScript libraries
 
 >`ua-parser-js` [(npm)](https://www.npmjs.com/package/ua-parser-js) is a JavaScript library to detect Browser, Engine, OS, CPU, and Device type/model from User-Agent data with relatively small footprint
-
+>`random-word-slugs` [(npm)](https://www.npmjs.com/package/random-word-slugs) A handy utility to create those random word slugs (e.g., generous-pink-biscuit) you see all over the place.
+>`uuid` [(npm)](https://www.npmjs.com/package/uuid) is a JavaScript implementation of the full UUID spec ([RFC9562](https://www.rfc-editor.org/rfc/rfc9562.html))
 
 Moving utility functions to API endpoints instead of importing libraries directly can significantly improve cross-language compatibility and provide access to runtimes or languages with less rich community support.
 
--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,44 @@
 const parser = require('ua-parser-js');
+import { v4 as uuidv4 } from 'uuid';
+import { generateSlug } from "random-word-slugs";
 
 export default {
-	async fetch(request, env, ctx): Promise<Response> {
+	async fetch(request, env, ctx,): Promise<Response> {
+		const url = new URL(request.url);
 
-		if (request.method === 'POST') {
-			const payload = await request.json();
-			let strToParse = (() => {
-				if (payload['user-agent']) {
-					return payload['user-agent'];
-				}
-				return request.headers.get('user-agent');
-			})()
-			let ua = parser(strToParse);
-			return new Response(JSON.stringify(ua));
-		} else if (request.method === 'GET') {
-			let ua = parser(request.headers.get('user-agent'));
-			return new Response(JSON.stringify(ua));
+		if (url.pathname === '/api/ua') {
+
+			if (request.method === 'POST') {
+				const payload = await request.json();
+				let strToParse = (() => {
+					if (payload['user-agent']) {
+						return payload['user-agent'];
+					}
+					return request.headers.get('user-agent');
+				})()
+				let ua = parser(strToParse);
+				return new Response(JSON.stringify(ua));
+			} else if (request.method === 'GET') {
+				let ua = parser(request.headers.get('user-agent'));
+				return new Response(JSON.stringify(ua));
+			}
+		}
+		if (url.pathname === '/api/uuid') {
+			return new Response(JSON.stringify({
+				success: true,
+				id: uuidv4()
+			}));
 		}
 
+		if (url.pathname === '/api/words') {
+			let length = 3
+			if (url.searchParams.has('length')) {
+				length = parseInt(url.searchParams.get('length') ?? '3');
+			}
+			return new Response(JSON.stringify({
+				success: true,
+				slug: generateSlug(length)
+			}));
+		}
 	},
 } satisfies ExportedHandler<Env>;
